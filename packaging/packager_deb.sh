85838c17ef5ec88149ca29156d791761031c61ba
Minh Quang <tnmquangcode@gmail.com>
2024-05-19 20:10:53 +0700

    Update packages for debian distro

--- packaging/packager_deb.sh
+++ packaging/packager_deb.sh
@@ -38,6 +38,7 @@ fi
 # pip3 install torch==1.8.1 torchvision==0.9.1
 pip3 install torch==2.2.0 torchvision==0.17.0
 # screen controlling
+pip3 install setuptools-rust
 pip3 install autopy==4.0.0
 pip3 install vncdotool==0.12.0
 apt-get -y install xdotool x11-apps imagemagick
