6c1b8eae867f7186164806a6a83d608a7b2737f0
Minh Quang <tnmquangcode@gmail.com>
2024-05-19 11:57:17 +0700

    Add a python venv handle in debian distro

--- packaging/packager_deb.sh
+++ packaging/packager_deb.sh
@@ -14,7 +14,7 @@ apt-get -y install python3 python3-coverage
 apt-get -y install python3-pil
 # pip dependencies (for dependencies not available as DEB)
 apt-get -y install gcc libx11-dev libxtst-dev python3-dev libpng-dev python3-pip 
-apt install python3.12-venv
+apt-get -y install python3.12-venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install --upgrade pip
