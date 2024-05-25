a7a70a98e3baffe9ce7512f8bcdc470ff2c58422
Minh Quang <tnmquangcode@gmail.com>
2024-05-25 12:56:10 +0700

    Fixing the venv in Debian distro

--- packaging/packager_deb.sh
+++ packaging/packager_deb.sh
@@ -16,9 +16,6 @@ apt-get -y install python3 python3-coverage
 apt-get -y install python3-pil
 # pip dependencies (for dependencies not available as DEB)
 apt-get -y install gcc libx11-dev libxtst-dev python3-dev libpng-dev python3-pip
-apt-get -y install python3-virtualenv
-virtualenv .venv
-source .venv/bin/activate
 pip3 install --upgrade pip
 pip3 install setuptools-rust
 apt-get -y install python3-numpy
--- packaging/packager_docker.sh
+++ packaging/packager_docker.sh
@@ -5,14 +5,31 @@ readonly packager=$(echo $install_variant | cut -d '.' -f 1)
 readonly distro=$(echo $install_variant | cut -d '.' -f 2)
 readonly version=$(echo $install_variant | cut -d '.' -f 3)
 
+# Ensure the project directory is set correctly
+project_dir=$(pwd)/..
+
+# Create a virtual environment in the project directory
+python3 -m venv $project_dir/venv
+
+# Install necessary dependencies in the virtual environment
+source $project_dir/venv/bin/activate
+pip install --upgrade pip
+
+# Run the Docker container with the virtual environment
 if [ "$packager" == "rpm" ]; then
     sudo docker run \
-            -e DISTRO="$distro" -e VERSION="$version" -e ROOT="/" \
-            -v $(pwd)/..:/guibot:rw $distro:$version \
+        -e DISTRO="$distro" -e VERSION="$version" -e ROOT="/" \
+        -v $project_dir:/guibot:rw $distro:$version \
+        /bin/bash -c "
+            source /guibot/venv/bin/activate && \
             /bin/bash /guibot/packaging/packager_rpm.sh
+        "
 elif [ "$packager" == "deb" ]; then
     sudo docker run \
-            -e DISTRO="$distro" -e VERSION="$version" -e ROOT="/" \
-            -v $(pwd)/..:/guibot:rw $distro:$version \
+        -e DISTRO="$distro" -e VERSION="$version" -e ROOT="/" \
+        -v $project_dir:/guibot:rw $distro:$version \
+        /bin/bash -c "
+            source /guibot/venv/bin/activate && \
             /bin/bash /guibot/packaging/packager_deb.sh
+        "
 fi
