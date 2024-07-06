5a9eaa6eb8a6ee48bda50a00c185e36576fe7cfe
Minh Quang <tnmquangcode@gmail.com>
2024-07-06 17:36:18 +0700

    Debian packager update

--- packaging/packager_deb.sh
+++ packaging/packager_deb.sh
@@ -5,6 +5,10 @@ readonly distro="${DISTRO:-ubuntu}"
 readonly distro_version="${VERSION:-xenial}"
 readonly distro_root="${ROOT:-$HOME}"
 
+activate_virtualenv() {
+    source "$distro_root/myVenv/bin/activate"
+}
+
 # deb dependencies
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -17,56 +21,64 @@ apt-get -y install python3-pil
 # pip dependencies (for dependencies not available as DEB)
 apt-get -y install gcc libx11-dev libxtst-dev python3-dev libpng-dev python3-pip
 apt-get -y install python3-virtualenv
+
 virtualenv myVenv
-source myVenv/bin/activate
+activate_virtualenv
 pip3 install --upgrade pip
 pip3 install setuptools-rust
 apt-get -y install python3-numpy
+
 if [[ $distro_version == "xenial" ]]; then
     export DISABLE_OPENCV=1
 else
     apt-get -y install python3-opencv
 fi
+
 # text matching
 if [[ $distro_version == "xenial" ]]; then
     export DISABLE_OCR=1
 else
     apt-get -y install tesseract-ocr libtesseract-dev
     apt-get -y install g++ pkg-config
-    # pip3 install pytesseract==0.3.4 tesserocr==2.5.1
     pip3 install pytesseract==0.3.10 tesserocr==2.7.0
 fi
+
 # deep learning
-# pip3 install torch==1.8.1 torchvision==0.9.1
 pip3 install torch==2.2.0 torchvision==0.17.0
 pip3 show torch torchvision
+
 # screen controlling
-# pip3 install autopy==4.0.0
-# export DISABLE_AUTOPY=1
+pip3 install autopy==4.0.0
 pip3 install vncdotool==0.12.0
 apt-get -y install xdotool x11-apps imagemagick
 apt-get -y install python3-tk scrot
+
 # TODO: install PyScreeze separately to replace the one PyAutoGUI will install which is incompatible with the current Pillow version 
 pip3 install pyscreeze==0.1.28
 pip3 install pyautogui
 echo check
 pip3 list
 apt-get -y install x11vnc
+
 # deb packaging and installing of current guibot source
 apt-get -y install dh-make dh-python debhelper python3-all devscripts
 NAME=$(sed -n 's/^Package:[ \t]*//p' "$distro_root/guibot/packaging/debian/control")
 CHANGELOG_REVS=($(sed -n -e 's/^guibot[ \t]*(\([0-9]*.[0-9]*\)-[0-9]*).*/\1/p' "$distro_root/guibot/packaging/debian/changelog"))
 VERSION=${CHANGELOG_REVS[0]}
 cp -r "$distro_root/guibot" "$distro_root/$NAME-$VERSION"
+
 cd "$distro_root/$NAME-$VERSION/packaging"
+activate_virtualenv
 debuild --no-tgz-check --no-lintian -i -us -uc -b
+
 cp ../${NAME}_${VERSION}*.deb "$distro_root/guibot"
 apt-get -y install "$distro_root/guibot/"${NAME}_${VERSION}*.deb
 rm -fr "$distro_root/$NAME-$VERSION"
+
 # virtual display
 apt-get -y install xvfb vim-common
 export DISPLAY=:99.0
-Xvfb :99 -screen 0 1024x768x24 &> /tmp/xvfb.log  &
+Xvfb :99 -screen 0 1024x768x24 &> /tmp/xvfb.log &
 touch /root/.Xauthority
 xauth add ${HOST}:99 . $(xxd -l 16 -p /dev/urandom)
 sleep 3  # give xvfb some time to start
@@ -76,7 +88,9 @@ apt-get install -y python3-pyqt5
 export XDG_RUNTIME_DIR="/tmp/runtime-root"
 mkdir /tmp/runtime-root
 chmod 0700 /tmp/runtime-root
+
 cd /usr/lib/python3/dist-packages/guibot/tests
+activate_virtualenv
 LIBPATH=".." COVERAGE="python3-coverage" sh coverage_analysis.sh
 
 exit 0
--- packaging/packager_rpm.sh
+++ packaging/packager_rpm.sh
@@ -40,7 +40,7 @@ dnf -y install xdotool xwd ImageMagick
 dnf -y install python3-tkinter scrot
 # TODO: install PyScreeze separately to replace the one PyAutoGUI will install which is incompatible with the current Pillow version
 pip3 install pyscreeze==0.1.28
-pip3 install pyautogui==0.9.53
+pip3 install pyautogui
 dnf -y install x11vnc
 
 # rpm packaging and installing of current guibot source
